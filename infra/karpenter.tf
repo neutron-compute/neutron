3134f60b80e9ad8f4b306ebf05811b5a5be04e68
Kuntal Basu <kuntalb@scribd.com>
2025-01-28 14:10:29 -0500

    uphrade underlying module vrsion for karpenter

--- infra/karpenter.tf
+++ infra/karpenter.tf
@@ -50,7 +50,7 @@ locals {
 
 module "karpenter" {
   source                        = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version                       = "20.2.1"
+  version                       = "20.33.1"
   cluster_name                  = module.eks.cluster_name
   enable_irsa                   = true
   irsa_oidc_provider_arn        = module.eks.oidc_provider_arn
